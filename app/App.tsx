b41428c7224fdf0fded83d34a9df03265b24269b
Loc <lokpham1220@gmail.com>
2025-04-06 12:22:32 +0700

    moi nhat hien tại

--- app/App.tsx
+++ app/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { OnboardingScreen } from './screens/onboarding/OnboardingScreen';
-import { LoginScreen } from './screens/auth/LoginScreen';
-import { ForgotPasswordScreen } from './screens/auth/ForgotPasswordScreen';
+import LoginScreen from './screens/auth/LoginScreen';
+import ForgotPasswordScreen from './screens/auth/ForgotPasswordScreen';
 
 const Stack = createNativeStackNavigator();
 
